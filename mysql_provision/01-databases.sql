100b3dda98552896350483b1c5e834158ac5ff99
Angel castelo <alcastelo@gmail.com>
2018-10-23 23:08:55 +0200

    Changed localhost for %

--- mysql_provision/01-databases.sql
+++ mysql_provision/01-databases.sql
@@ -5,9 +5,9 @@ CREATE DATABASE IF NOT EXISTS `sonarqube`;
 # create root user and grant rights
 
 flush privileges;
-CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
-GRANT ALL ON gogs.* TO 'dev'@'localhost';
+CREATE USER 'dev'@'%' IDENTIFIED BY 'dev';
+GRANT ALL ON gogs.* TO 'dev'@'%';
 
 flush privileges;
-CREATE USER 'sonar'@'localhost' IDENTIFIED BY 'sonar';
-GRANT ALL ON sonar.* TO 'sonar'@'localhost';
+CREATE USER 'sonar'@'%' IDENTIFIED BY 'sonar';
+GRANT ALL ON sonar.* TO 'sonar'@'%';
