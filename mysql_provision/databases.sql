5e791e03d8cbcc71bed199472520d4721491d006
Angel castelo <alcastelo@gmail.com>
2018-10-23 20:59:37 +0200

    Added database provision

---
+++ mysql_provision/databases.sql
@@ -0,0 +1,11 @@
+# create databases
+CREATE DATABASE IF NOT EXISTS `gogs`;
+CREATE DATABASE IF NOT EXISTS `sonarqube`;
+
+# create root user and grant rights
+CREATE USER 'root'@'localhost' IDENTIFIED BY 'root';
+GRANT ALL ON *.* TO 'root'@'%';
+CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
+GRANT ALL ON gogs.* TO 'dev'@'%';
+CREATE USER 'sonar'@'localhost' IDENTIFIED BY 'sonar';
+GRANT ALL ON sonar.* TO 'sonar'@'%';
