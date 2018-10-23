514ba877b91ecbfaa648eebe7ad7860c94df48fc
Angel castelo <alcastelo@gmail.com>
2018-10-23 22:29:40 +0200

    root user auto created

--- mysql_provision/databases.sql
+++ mysql_provision/databases.sql
@@ -3,8 +3,7 @@ CREATE DATABASE IF NOT EXISTS `gogs`;
 CREATE DATABASE IF NOT EXISTS `sonarqube`;
 
 # create root user and grant rights
-CREATE USER 'root'@'localhost' IDENTIFIED BY 'root';
-GRANT ALL ON *.* TO 'root'@'%';
+
 CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
 GRANT ALL ON gogs.* TO 'dev'@'%';
 CREATE USER 'sonar'@'localhost' IDENTIFIED BY 'sonar';
