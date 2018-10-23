c77118c8ea47663eaf5d2c1813acfc0c52c9c177
Angel castelo <alcastelo@gmail.com>
2018-10-23 22:57:28 +0200

    Remove drop user

--- mysql_provision/01-databases.sql
+++ mysql_provision/01-databases.sql
@@ -4,12 +4,10 @@ CREATE DATABASE IF NOT EXISTS `sonarqube`;
 
 # create root user and grant rights
 
-drop user 'dev'@'localhost';
 flush privileges;
 CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
 GRANT ALL ON gogs.* TO 'dev'@'localhost';
 
-drop user 'sonar'@'localhost';
 flush privileges;
 CREATE USER 'sonar'@'localhost' IDENTIFIED BY 'sonar';
 GRANT ALL ON sonar.* TO 'sonar'@'localhost';
